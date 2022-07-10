4564d6e57c2b894d22553d5f0adba621bb8a86a2
ks315-kim <ks315.kim@samsung.com>
2022-07-10 17:31:12 +0900

    chard

---
+++ pages/link/index.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+
+import { Button } from "antd";
+import "antd/dist/antd.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Prediction22</h1>
+      <Button type="primary">Button</Button>
+      <Button type="primary">Button</Button>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+};
+
+export default Home;
